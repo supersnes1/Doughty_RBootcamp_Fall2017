7f8aee5dbc59ba050806dd756bac85f47c9dd0d0
supersnes1 <emdoughty@g.cula.edu>
2017-09-19 22:07:34 -0700

    Completed Section 1 to 4
    Completed section 1 to 4 of the R tutorial introduciton assignment.
    
    https://kingaa.github.io/R_Tutorial/#how-to-use-this-document

--- RBootcampCode.R
+++ RBootcampCode.R
@@ -1,5 +1,89 @@
 #Bootcamp Code
 
 library(ape)
-library(gieger)
+library(geiger)
 library(ggplot2)
+
+#Interactive Calculations
+2+2
+
+a <- 2+2
+a
+
+a <- 2+2; a
+
+a <- 3*(4+5)
+a
+
+a <- 2+2
+2+2 -> a
+a = 2+2
+a
+
+x <- 5
+y <- 2
+z1 <- x*y
+z2 <- x/y
+z3 <- x^y
+z1;z2;z3
+
+z3 <- 2*x^y
+z3
+
+A<-3
+C<-(A+2*sqrt(A))/(A+5*sqrt(A)); C
+
+C<- A+2*sqrt(A)/A+5*sqrt(A)
+C
+
+C <- A + 2*(sqrt(A)/A) + 5*sqrt(A)
+C
+
+b <- 12-4/2^3
+b
+
+b <- (12-4)/2^3
+b
+
+b <- -1^2
+b
+
+b <- (-1)^2
+b
+
+b <- 12 - (4/(2^3))
+b
+
+b <- 12 - 4/(2^3)
+b
+
+#4.0.0.0.1 Exercise
+# 1
+(2^7)/(2^7-1)
+
+#2
+(1-(1/(2^7)))^-1
+
+#3
+1+0.2
+
+#4
+1+0.2+((0.2^2)/2)
+
+#5
+1+0.2+(((0.2)^2)/2)+(((0.2)^3)/6)
+
+#6
+exp(0.2)
+
+#7
+x1 <- 1
+x2 <- 2
+x <- x1
+(1/sqrt(2*pi))*exp((-x^2)/2)
+
+x <- x2
+(1/sqrt(2*pi))*exp((-x^2)/2)
+
+dnorm(x=c(1,2))
+
