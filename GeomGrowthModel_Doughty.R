f009a0207e08a41506ed4f468e97091c7dbf2c0b
supersnes1 <emdoughty@g.cula.edu>
2017-09-27 11:11:30 -0700

    Update to Code from Lecture Activities

--- GeomGrowthModel_Doughty.R
+++ GeomGrowthModel_Doughty.R
@@ -20,7 +20,7 @@ for(tt in time.seq)
 return(NN)
 }
 
-disc.logMod <- function(start.pop = 1, num.years = 2, Rval = 1,rd = 0.5,K = 1)
+disc.logMod <- function(start.pop = 1, num.years = 2,rd = 0.5,K = 10)
 {
   time.seq <- seq(1,num.years,1)
 NN <- rep(0,length.out = length(time.seq))
@@ -42,21 +42,76 @@ return(NN)
 
 #initialize variables and global vector for storage
 Rval <- 1.05
-years <- 10
+years <- 25
 start.pop <- 100
-KK = 1000 #must be greater than start.pop
-rd=0.75
+KK = 10000 #must be greater than start.pop
+rd=0.4
+timeline <- seq(0,years,1)
 
 #call function and assign result to vector
 pop.growth <- geom.growthMod(start.pop = start.pop, num.years = years,Rval = Rval)
 
 #print result and plot is desired
 cat(pop.growth)
-timeline <- seq(0,years,1)
 plot(pop.growth,timeline, #xlim = c(100,2000),
      xlab="Population (N)", ylab = "years (t)")
 
+#Discrete Log Model
 #N(t+1)=N(t)(1+rd(1-N(t)/K))
-pop.growthDisc <- disc.logMod(start.pop = start.pop, num.years = years,Rval = Rval,rd = rd,K= KK)
+pop.growthDisc <- disc.logMod(start.pop = start.pop, num.years = years,rd = rd,K= KK)
+
+#print result and plot is desired
+cat(pop.growth)
 plot(pop.growthDisc,timeline, #xlim = c(100,2000),
      xlab="Population (N)", ylab = "years (t)")
+abline(v=KK,col="black",lty=3)
+abline(a=pop.growth,b=timeline)
+
+#plot of model summary output
+#define parameter values
+  #see how rd affects population at 5 years
+Rval <- 1.05
+years <- 5
+start.pop <- 100
+KK <- 10000 #must be greater than start.pop
+timeline <- seq(0,years,1)
+
+#for aprmeters of intrest make vector of values you want to consider
+rdmin <- 0.01
+rdmax <- 1
+rdInterLeng <- 0.01
+rd.vec <- seq(rdmin,rdmax,rdInterLeng)
+
+#initialise vector to hold summary values
+result.vec <- rep(0, length.out = length(rd.vec))
+
+#use a for loop to repreatedly run model and plot ouput
+  #loop over list of values you want to use for the param varying
+#each time trhough the loop, run model with current parameter
+#values andstore the summary values in the ith element of results vector
+for(ii in seq(1,length(rd.vec),1))
+{
+  pop.growth <- disc.logMod(start.pop = start.pop, num.years = years,rd = rd.vec[ii],K= KK)
+  result.vec[ii] <- pop.growth[years]
+}
+
+plot(rd.vec,result.vec, #xlim = c(100,2000),
+     xlab="rd", ylab = "Population (N)")
+
+#Continuous Time Models
+#dN/dt = rN(1-N/K)
+#lsoda()
+install.packages("deSolve")
+library(deSolve)
+
+expGrowthODE <-function(tt,NN,parts)
+{
+  derivs <- pars['rr']*NN
+  return(list(derivs))
+}
+init <- 1
+tseq <- seq(0,20,by=0.01)
+pars <- c(rr=0.1)
+output <- lsoda(init, tseq, expGrowthODE,pars)
+
+plot(output,tseq)
